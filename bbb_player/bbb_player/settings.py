19eb971601b6dd1fe5a573a5a572e9207dded1cf
myOmikron <accounts@omikron.dev>
2021-05-19 23:52:23 +0200

    Added static root

--- bbb_player/bbb_player/settings.py
+++ bbb_player/bbb_player/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/html"
 
 RCP_SECRET = "change_me"
 RCP_TIMEDELTA = 5
