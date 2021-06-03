2f212bb4de257d9e4886f143e468023ab34df79b
myOmikron <accounts@omikron.dev>
2021-06-03 18:54:54 +0200

    Added post publish script for bigbluebutton

---
+++ moveRecording.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+require "trollop"
+require File.expand_path('../../../lib/recordandplayback', __FILE__)
+
+opts = Trollop::options do
+  opt :meeting_id, "Meeting id to archive", :type => String
+  opt :format, "Playback format name", :type => String
+end
+meeting_id = opts[:meeting_id]
+
+system("/bin/bash moveRecording.sh " + meeting_id)
+
+exit 0
---
+++ moveRecording.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+####
+# Usage:
+#   Change vars below
+#   Execute
+###
+
+player_server="https://10.1.2.3"
+rcp_secret="change_me"
+rsync_path="bbb-player:/var/www/todo/"
+
+# Do not touch!
+meeting_id=$1
+published_files="/var/bigbluebutton/published/presentation/$meeting_id/"
+
+rsync -a "$published_files" $rsync_path
+curl -H "Content-Type: application/json" -d '{"internalMeetingId": "'$meeting_id'", "checksum": "'$(python -c "import rc_protocol; print(rc_protocol.get_checksum({\"internalMeetingId\": \"$meeting_id\"}, \"$rcp_secret\", \"updatePublishState\"))")'"}' "$player_server/api/v1/updatePublishState"
+
+# Delete files locally
+rm -r "$published_files"
