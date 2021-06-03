2762c7ea3b4cfcaf16ccff0c90c830019183d32e
myOmikron <accounts@omikron.dev>
2021-06-03 21:16:13 +0200

    Added views for gathering and deleting recordings

--- bbb_player/api/views.py
+++ bbb_player/api/views.py
@@ -1,7 +1,12 @@
 import json
+import os
+import re
+import shlex
+import shutil
+import subprocess
 
 from django import views
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from rc_protocol.rc_protocol import validate_checksum
 
@@ -30,10 +35,19 @@ class GetRecordingsView(views.View):
             return JsonResponse({"success": False, "message": decoded["message"]}, status=decoded["status"])
         if not isinstance(decoded["recordings"], list):
             return {"success": False, "message": "Parameter recordings is not a list", "status": 400}
-        # TODO:
-        # If empty: Return all
-        # If one: Gather metadata from one
-        # If multiple: Gather metadata from those, concatenate
+        if len(decoded["recordings"]) == 0:
+            files = [os.path.join(x, "recording.xml") for x in os.listdir("/var/bigbluebutton/published/presentation/")]
+        else:
+            files = [os.path.join("/var/bigbluebutton/published/presentation", x, "recording.xml") for x in decoded["recordings"]]
+        try:
+            response = ""
+            for file in files:
+                if os.path.exists(file) and os.path.isfile(file):
+                    with open(file) as fh:
+                        response += fh.read() + "\n"
+            return HttpResponse(response)
+        except Exception:
+            return HttpResponse("")
 
 
 class DeleteRecordingsView(views.View):
@@ -45,7 +59,10 @@ class DeleteRecordingsView(views.View):
             return {"success": False, "message": "Parameter recordings is not a list", "status": 400}
         if len(decoded["recordings"]) == 0:
             return JsonResponse({"success": False, "message": "Parameters recordings has to contain an element"})
-        # TODO: Try deleting all recordings
+        for recording in decoded["recordings"]:
+            if re.match(r"^[\w\d]+-[\w\d]+$", recording):
+                shutil.rmtree(os.path.join("/var/bigbluebutton/published/presentation/", recording))
+        return JsonResponse({"success": True, "message": "Reordings has been deleted"})
 
 
 class UpdatePublishStateView(views.View):
@@ -54,4 +71,6 @@ class UpdatePublishStateView(views.View):
         if not decoded["success"]:
             return JsonResponse({"success": False, "message": decoded["message"]}, status=decoded["status"])
         internal_meeting_id = decoded["internalMeetingId"]
-        # TODO: Start Recording processor
+        cmd = shlex.split(f"python3 /home/bbb-player/bbb-player/bbb_player/worker/main.py -i {internal_meeting_id} -H {settings.HOSTNAME}")
+        subprocess.Popen(cmd)
+        return HttpResponse("200")
