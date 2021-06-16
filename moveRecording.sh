171b9d937ef28d5650da845523696308a91e1adb
myOmikron <accounts@omikron.dev>
2021-06-16 20:00:56 +0200

    Updated rc_protocol to new version

--- moveRecording.sh
+++ moveRecording.sh
@@ -15,7 +15,7 @@ meeting_id=$1
 published_files="/var/bigbluebutton/published/presentation/$meeting_id/"
 
 rsync -a "$published_files" $rsync_path"/$meeting_id"
-curl -H "Content-Type: application/json" -d '{"internalMeetingId": "'$meeting_id'", "checksum": "'$(python3 -c "import rc_protocol; print(rc_protocol.get_checksum({\"internalMeetingId\": \"$meeting_id\"}, \"$rcp_secret\", \"updatePublishState\"))")'"}' "$player_server/api/v1/updatePublishState"
+curl -H "Content-Type: application/json" -d '{"internalMeetingId": "'$meeting_id'", "checksum": "'$(python3 -c "import rc_protocol; print(rc_protocol.get_checksum({\"internalMeetingId\": \"$meeting_id\"}, \"$rcp_secret\", salt=\"updatePublishState\"))")'"}' "$player_server/api/v1/updatePublishState"
 
 # Delete files locally
 rm -r "$published_files"
